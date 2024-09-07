cb8d58cf441111cb2362c95f2a3c82d71d2c736c
Saad Adel <saad567567aa@gmail.com>
2024-09-07 13:32:17 +0300

    Refactor import paths and add new login screen ent.

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -1,4 +1,4 @@
-import 'package:al_dahabiya/feature/Auth/presentation/view/login_screen.dart';
+import 'package:al_dahabiya/feature/Auth/featuers/login/presentation/view/login_screen.dart';
 import 'package:al_dahabiya/feature/splash/presentation/view/splash_screen.dart';
 import 'package:al_dahabiya/feature/splash/presentation/view_model/cubit/splash_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
