15058baeb5620d7904e3c22dc343da7907104471
Saad Adel <saad567567aa@gmail.com>
2024-11-05 17:21:07 +0200

    Refactor OrderRequestScreen to handle loading states and navigation
    - Added a loading state (`isLoading`) to manage the UI state during asynchronous operations.
    - Updated the `BlocListener` to set `isLoading` to true when the address data is being fetched and to false when the operation completes or fails.
    - Wrapped the main content in a `Stack` to overlay a loading indicator when `isLoading` is true.
    - Imported `go_router` and `AppRouters` to handle navigation when the "Cancel Orders" button is pressed.
    - Updated the "Cancel Orders" button to clear the cart table and navigate to the first route using `GoRouter`.

--- lib/feature/order_request/presentation/view/order_screen.dart
+++ lib/feature/order_request/presentation/view/order_screen.dart
@@ -14,7 +14,9 @@ import 'package:drop_down_list/model/selected_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/app_router.dart';
 import '../../../address/data/models/user_address_model.dart';
 import '../../../address/presentation/view/widgets/drop_down_function.dart';
 import '../../../address/presentation/view/widgets/semulated_drop_down.dart';
@@ -34,6 +36,7 @@ List<AddressData>? addressData;
 String orderAddressId = '0';
 String orderAddress = "";
 double shipingCost = 0.0;
+bool isLoading = false;
 
 class _OrderRequestScreenState extends State<OrderRequestScreen> {
   @override
@@ -52,170 +55,197 @@ class _OrderRequestScreenState extends State<OrderRequestScreen> {
       ],
       child: BlocListener<AddressCubit, AddressState>(
         listener: (context, state) {
-          if (state is GetUserAddressSuccess) {
-            addressData = state.addressData;
+          if (state is GetUserAddressLoading) {
+            setState(() {
+              isLoading = true;
+            });
+          } else if (state is GetUserAddressSuccess) {
+            setState(() {
+              addressData = state.addressData;
+              isLoading = false;
+            });
+          } else if (state is GetUserAddressFailure) {
+            setState(() {
+              isLoading = false;
+            });
           }
         },
         child: Scaffold(
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-            child: Column(
-              children: [
-                SafeArea(child: SizedBox(height: 15.h)),
-                PageTitleBar(isTitlePade: true, pageTitle: 'عملية الشراء'),
-                SizedBox(height: 15.h),
-                Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(children: [
-                      const Icon(
-                        Icons.location_on_outlined,
-                        color: Colors.red,
-                      ),
-                      const SizedBox(width: 10),
-                      InkWell(
-                        onTap: () {
-                          log(addressData.toString());
-                          final List<SelectedListItem> addressItems =
-                              addressData!
-                                  .map((add) => SelectedListItem(
-                                        address: '',
-                                        name: add.address,
-                                        value: add.id.toString(),
-                                        shapingCost:
-                                            add.shippingCost.toString(),
-                                      ))
-                                  .toList();
-                          dropDown(
-                            title: 'اختر عنوانك الحالي من هنا',
-                            data: addressItems,
-                            onSelected: (SelectedListItem selectedItem) {
-                              setState(() {
-                                orderAddress = selectedItem.name;
-                                orderAddressId = selectedItem.value;
-                                shipingCost =
-                                    double.parse(selectedItem.shapingCost);
-                              });
-                            },
-                          ).showModal(context);
-                        },
-                        child: const SemulatedDropDown(
-                          title: 'عنوان الطلب',
-                        ),
-                      ),
-                    ]),
-                  ),
-                ),
-                SizedBox(height: 15.h),
-                DataRowItem(
-                  data: orderAddress,
-                  icon: const Icon(Icons.location_on_outlined),
-                ),
-                SizedBox(height: 20.h),
-                const Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: CategoryTitleRow(
-                    isBoold: true,
-                    categoryName: 'الطلبيات',
-                  ),
-                ),
-                SizedBox(height: 15.h),
-                BlocBuilder<CartCubit, CartState>(
-                  builder: (context, state) {
-                    if (state is CartLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is CartFailure) {
-                      return Center(
-                        child: Column(
-                          children: [
-                            Text(
-                                'Failed to load cart items: ${state.errorMessage}'),
-                            SizedBox(height: 10.h),
-                          ],
-                        ),
-                      );
-                    } else if (state is CartSuccess) {
-                      final cartItems = state.cartItems;
-                      if (cartItems.isEmpty) {
-                        return const Center(
-                          child: Text('No items in your cart',
-                              style: TextStyle(fontSize: 18)),
-                        );
-                      }
-
-                      return SizedBox(
-                        height: 150.h,
-                        child: Directionality(
-                          textDirection: TextDirection.rtl,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: cartItems.length,
-                            itemBuilder: (context, index) {
-                              return OrderRequestItem(
-                                  cartItem: cartItems[index]);
-                            },
+          body: Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+                child: Column(
+                  children: [
+                    SafeArea(child: SizedBox(height: 15.h)),
+                    PageTitleBar(isTitlePade: true, pageTitle: 'عملية الشراء'),
+                    SizedBox(height: 15.h),
+                    Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(children: [
+                          const Icon(
+                            Icons.location_on_outlined,
+                            color: Colors.red,
                           ),
-                        ),
-                      );
-                    }
-                    return const SizedBox.shrink();
-                  },
-                ),
-                SizedBox(height: 15.h),
-                BlocBuilder<CartCubit, CartState>(
-                  builder: (context, state) {
-                    if (state is CartLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is CartFailure) {
-                      return Center(
-                        child: Text(
-                            'Failed to load total price: ${state.errorMessage}'),
-                      );
-                    } else if (state is CartSuccess) {
-                      return FatoraDetailsRow(
-                        fatoraCount: state.totalPrice,
-                        fatoraTitle: 'سعر الطلبات',
-                      );
-                    }
-                    return const SizedBox.shrink();
-                  },
-                ),
-                SizedBox(height: 15.h),
-                FatoraDetailsRow(
-                    fatoraCount: shipingCost, fatoraTitle: 'سعر الشحن'),
-                SizedBox(height: 15.h),
-                BlocBuilder<CartCubit, CartState>(
-                  builder: (context, state) {
-                    if (state is CartSuccess) {
-                      return PayRow(
-                        addressId: int.parse(orderAddressId),
-                        shipingCost: shipingCost,
-                        cartItemsInOrder: state.cartItems,
-                      );
-                    }
-                    return const SizedBox.shrink();
-                  },
-                ),
-                SizedBox(height: 15.h),
-                SizedBox(
-                  width: 250.w,
-                  height: 45.h,
-                  child: Builder(builder: (context) {
-                    return ElevatedButton(
-                      onPressed: () => context.read<CartCubit>().clearTable(),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFF6F3F4),
+                          const SizedBox(width: 10),
+                          InkWell(
+                            onTap: () {
+                              log(addressData.toString());
+                              final List<SelectedListItem> addressItems =
+                                  addressData!
+                                      .map((add) => SelectedListItem(
+                                            address: '',
+                                            name: add.address,
+                                            value: add.id.toString(),
+                                            shapingCost:
+                                                add.shippingCost.toString(),
+                                          ))
+                                      .toList();
+                              dropDown(
+                                title: 'اختر عنوانك الحالي من هنا',
+                                data: addressItems,
+                                onSelected: (SelectedListItem selectedItem) {
+                                  setState(() {
+                                    orderAddress = selectedItem.name;
+                                    orderAddressId = selectedItem.value;
+                                    shipingCost =
+                                        double.parse(selectedItem.shapingCost);
+                                  });
+                                },
+                              ).showModal(context);
+                            },
+                            child: const SemulatedDropDown(
+                              title: 'عنوان الطلب',
+                            ),
+                          ),
+                        ]),
                       ),
-                      child: const Text(
-                        'الغاء الطلبيات',
-                        style: TextStyle(fontSize: 22, color: Colors.black),
+                    ),
+                    SizedBox(height: 15.h),
+                    DataRowItem(
+                      data: orderAddress,
+                      icon: const Icon(Icons.location_on_outlined),
+                    ),
+                    SizedBox(height: 20.h),
+                    const Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: CategoryTitleRow(
+                        isBoold: true,
+                        categoryName: 'الطلبيات',
                       ),
-                    );
-                  }),
+                    ),
+                    SizedBox(height: 15.h),
+                    BlocBuilder<CartCubit, CartState>(
+                      builder: (context, state) {
+                        if (state is CartLoading) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (state is CartFailure) {
+                          return Center(
+                            child: Column(
+                              children: [
+                                Text(
+                                    'Failed to load cart items: ${state.errorMessage}'),
+                                SizedBox(height: 10.h),
+                              ],
+                            ),
+                          );
+                        } else if (state is CartSuccess) {
+                          final cartItems = state.cartItems;
+                          if (cartItems.isEmpty) {
+                            return const Center(
+                              child: Text('No items in your cart',
+                                  style: TextStyle(fontSize: 18)),
+                            );
+                          }
+
+                          return SizedBox(
+                            height: 150.h,
+                            child: Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: cartItems.length,
+                                itemBuilder: (context, index) {
+                                  return OrderRequestItem(
+                                      cartItem: cartItems[index]);
+                                },
+                              ),
+                            ),
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                    ),
+                    SizedBox(height: 15.h),
+                    BlocBuilder<CartCubit, CartState>(
+                      builder: (context, state) {
+                        if (state is CartLoading) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (state is CartFailure) {
+                          return Center(
+                            child: Text(
+                                'Failed to load total price: ${state.errorMessage}'),
+                          );
+                        } else if (state is CartSuccess) {
+                          return FatoraDetailsRow(
+                            fatoraCount: state.totalPrice,
+                            fatoraTitle: 'سعر الطلبات',
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                    ),
+                    SizedBox(height: 15.h),
+                    FatoraDetailsRow(
+                        fatoraCount: shipingCost, fatoraTitle: 'سعر الشحن'),
+                    SizedBox(height: 15.h),
+                    BlocBuilder<CartCubit, CartState>(
+                      builder: (context, state) {
+                        if (state is CartSuccess) {
+                          return PayRow(
+                            addressId: int.parse(orderAddressId),
+                            shipingCost: shipingCost,
+                            cartItemsInOrder: state.cartItems,
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                    ),
+                    SizedBox(height: 15.h),
+                    SizedBox(
+                      width: 250.w,
+                      height: 45.h,
+                      child: Builder(builder: (context) {
+                        return ElevatedButton(
+                          onPressed: () {
+                            context.read<CartCubit>().clearTable();
+                            GoRouter.of(context).push(AppRouters.kFirstRoute);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFF6F3F4),
+                          ),
+                          child: const Text(
+                            'الغاء الطلبيات',
+                            style: TextStyle(fontSize: 22, color: Colors.black),
+                          ),
+                        );
+                      }),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              if (isLoading)
+                Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+            ],
           ),
         ),
       ),
