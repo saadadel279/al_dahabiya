a1a1a35efafea8f87fd47796319e2fe98813ca5d
Saad Adel <saad567567aa@gmail.com>
2024-11-25 04:33:43 +0200

    Add Firebase Messaging and Local Notifications
    Integrate Firebase Messaging and Local Notifications for handling push notifications in the Android app. This includes adding necessary dependencies, configuring Firebase services, and initializing notification services in the main application file.

---
+++ lib/core/services/notification/local_notification/local_notification.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  static StreamController<NotificationResponse> streamController =
+      StreamController();
+  static onTap(NotificationResponse notificationResponse) {
+    streamController.add(notificationResponse);
+  }
+
+  static Future init() async {
+    InitializationSettings settings = const InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/launcher_icon'),
+      iOS: DarwinInitializationSettings(),
+    );
+    flutterLocalNotificationsPlugin.initialize(
+      settings,
+      onDidReceiveNotificationResponse: onTap,
+      onDidReceiveBackgroundNotificationResponse: onTap,
+    );
+  }
+
+  static void showBasicNotification(RemoteMessage message) async {
+    AndroidNotificationDetails android = const AndroidNotificationDetails(
+      'id 1',
+      'basic notification',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    NotificationDetails details = NotificationDetails(
+      android: android,
+    );
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      message.notification?.title,
+      message.notification?.body,
+      details,
+    );
+  }
+}
